fb93faf052a9cfd647f7147c85cebf86fc264b6c
Patrick Flinner <ptflinner@yahoo.com>
2018-03-09 18:34:06 -0800

    Finished more functions

--- functions.m
+++ functions.m
@@ -32,8 +32,12 @@ r33 = cos(pi*b/180)*cos(pi*c/180);
 A = [atan2(r32/cos(b*pi/180),r33/cos(b*pi/180));atan2(-r31, sqrt(r11^2+r21^2));atan2(r21/cos(b*pi/180),r11/cos(b*pi/180))]*(180/pi);
 endfunction
 
-function [d]= distance(a,b)
+function [d]= distance3D(a,b)
 d=sqrt(power((a(1)+b(1)),2)+power((a(2)+b(2)),2)+power((a(3)+b(3)),2))
+endfunction 
+
+function [d]= distance2D(a,b)
+d=sqrt(power((a(1)+b(1)),2)+power((a(2)+b(2)),2))
 endfunction 
 
 function [Angles]= recAng(A)
@@ -48,4 +52,22 @@ function [Angles]= recRad(A)
   alpha=atan2(A(2)/(cos(beta)),A(1)/cos(beta))
   gamma=atan2(A(6)/cos(beta),A(9)/cos(beta))
   Angles=[beta;alpha;gamma]
+endfunction
+
+function [Rotate]= rotateCoord2D(A,a)
+  Rotate=[cos(pi*a/180), sin(pi*a/180);-sin(pi*a/180),cos(pi*a/180)]*A
+endfunction
+function [RotMatrix]=rotationMatrix2D(a)
+  RotMatrix=[cos(pi*a/180), sin(pi*a/180);-sin(pi*a/180),cos(pi*a/180)]
+endfunction
+function [Rotate]=rotateVector2D(A,a)
+    Rotate=[cos(pi*a/180), -sin(pi*a/180);sin(pi*a/180),cos(pi*a/180)]*A
+endfunction
+
+function [Translation]=translationMatrix2D(h,k)
+  Translation=[1,0,h;0,1,k;0,0,1]
+endfunction
+
+function [Translation]=translationMatrix3D(h,k,i)
+  Translation=[1,0,0,h;0,1,0,k;0,0,1,i;0,0,0,1]
 endfunction
